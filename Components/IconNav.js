572dcba69bece0dd185ceff7ad54ebe20d1cc302
Bobby Boyd <bboyd2008@gmail.com>
2019-08-15 22:35:45 -0700

    fixed names

--- Components/IconNav.js
+++ Components/IconNav.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Logo from "../images/logo.png";
 import BookIcon from "../images/BookIcon.png";
-import Calc from "../images/calc.png";
-import Light from "../images/light.png";
-import Mug from "../images/mug.png";
-import Pencil from "../images/pencil.png";
+import Calc from "../images/Calc.png";
+import Light from "../images/Light.png";
+import Mug from "../images/Mug.png";
+import Pencil from "../images/Pencil.png";
 import Link from "next/link";
 import PBtn from "./PBtn";
 const IconNav = props => (
