3bf3a33c076e8b39e57481ff629a48b55bfcc33d
Bobby Boyd <bboyd2008@gmail.com>
2019-08-17 19:21:46 -0700

    front-end css

--- Components/TitleComponent.js
+++ Components/TitleComponent.js
@@ -2,10 +2,10 @@ import React from "react";
 
 const TitleComponent = props => {
   return (
-    <>
+    <div className="title-component">
       <img src={props.img} alt="" />
       <h1 className="medium-padding-bottom">{props.h1}</h1>
-    </>
+    </div>
   );
 };
 
--- pages/teacher-freebies.js
+++ pages/teacher-freebies.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import PBtn from "../Components/PBtn";
+import SBtn from "../Components/SBtn";
 import Input from "../Components/Input";
 import Header from "../Components/Header";
 import api from "../utils/api";
 import Link from "next/link";
 import "isomorphic-fetch";
+import Footer from "../Components/Footer";
+import ContainerFluid from "../Components/FormatComponents/ContainerFluid";
+import Container from "../Components/FormatComponents/Container";
+import Columns from "../Components/FormatComponents/Columns";
+import Column from "../Components/FormatComponents/Column";
+import Layout from "../Components/Layout/Layout";
+import Backpack from "../images/E.png";
+import TitleComponent from "../Components/TitleComponent";
 const Freebies = props => {
   const logout = async () => {
     await api.logout();
@@ -12,53 +21,71 @@ const Freebies = props => {
     window.location.reload();
   };
   return (
-    <div className="freebies">
-      <Header />
-      <div className="container">
-        {props.user ? (
-          <div className="column">
-            <PBtn onClick={logout}>Logout</PBtn>
-            <div className="container">
-              <div className="columns">
+    <Layout>
+      <div className="freebies">
+        <Header />
+        <Container>
+          {props.user ? (
+            <Container className="teacher-freebies-container">
+              <PBtn onClick={logout}>Logout</PBtn>
+              <TitleComponent img={Backpack} h1="Freebies" />
+              <Columns className="is-multiline is-centered">
                 {props.freebies.map((freebie, index) => {
                   return (
-                    <div className="column is-3">
-                      <img src={freebie.img} alt="" />
-                      <Link href={freebie.path}>
-                        <a>Download</a>
-                      </Link>
-                    </div>
+                    <Column className="is-3 has-text-centered">
+                      <div className="card">
+                        <div className="card-image">
+                          <figure className="image">
+                            <img src={freebie.img} alt={freebie.originalname} />
+                          </figure>
+                        </div>
+
+                        <div className="card-body">
+                          <h2 className="card-title">{freebie.originalname}</h2>
+                        </div>
+                        <div className="card-footer">
+                          <p className="card-footer-item">
+                            <Link href={freebie.path}>
+                              <a>
+                                <i className="fas fa-download"></i> Download
+                              </a>
+                            </Link>
+                          </p>
+                        </div>
+                      </div>
+                    </Column>
                   );
                 })}
+              </Columns>
+            </Container>
+          ) : (
+            <div className="columns is-centered is-multiline">
+              <div className="column is-12 heading">
+                <h1>Make sure to login with the info that I sent you.</h1>
+              </div>
+              <div className="column is-8 has-text-centered">
+                <form className="login-form" action="/api/login" method="POST">
+                  <Input
+                    className="form-control"
+                    type="text"
+                    placeholder="Username"
+                    name="username"
+                  />
+                  <Input
+                    className="form-control"
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                  />
+                  <PBtn type="submit">Get Freebies</PBtn>
+                </form>
               </div>
             </div>
-          </div>
-        ) : (
-          <div className="columns is-centered is-multiline">
-            <div className="column is-12 heading">
-              <h1>Make sure to login with the info that I sent you.</h1>
-            </div>
-            <div className="column is-8 has-text-centered">
-              <form className="login-form" action="/api/login" method="POST">
-                <Input
-                  className="form-control"
-                  type="text"
-                  placeholder="Username"
-                  name="username"
-                />
-                <Input
-                  className="form-control"
-                  type="password"
-                  placeholder="Password"
-                  name="password"
-                />
-                <PBtn type="submit">Get Freebies</PBtn>
-              </form>
-            </div>
-          </div>
-        )}
+          )}
+        </Container>
+        <Footer />
       </div>
-    </div>
+    </Layout>
   );
 };
 
