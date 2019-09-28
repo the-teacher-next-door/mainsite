d502e06a30bc98a9c53455fdf07834d9fc5316e5
Bobby Boyd <bboyd2008@gmail.com>
2019-09-28 12:26:12 -0700

    meta info

--- Components/Layout/Layout.js
+++ Components/Layout/Layout.js
@@ -13,6 +13,13 @@ const Layout = props => {
         ></link>
         <script src="https://kit.fontawesome.com/33b13ebe41.js"></script>
         <script src="//assets.pinterest.com/js/pinit.js"></script>
+        <meta
+          name="description"
+          content="Creative Ideas and Quality Common Core Aligned Teaching Materials From My Classroom To Yours"
+        />
+        <meta name="author" content="Jennifer Larson" />
+        <meta name="title" content="The Teacher Next Door - Creative Ideas From My Classroom To Yours"/>
+        <meta name="keywords" content="Common Core, reading, Common Core standards English Language Arts, ELA, reading strategies, teaching ideas, teaching strategies, reading comprehension, theme, main idea, inferences, characters, close reading, figurative language, teachers pay teachers, test prep, classroom organization, classroom management, writing, persuasive writing, paragraph writing, compare and contrast, cause and effect, informational text structures, task cards, mentor texts, third grade, fourth grade, fifth grade, elementary education, primary education teacher blog" />
       </Head>
       {props.children}
       {/* <style jsx global>{`
