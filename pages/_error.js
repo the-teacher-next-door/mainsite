33f578d9f7963ffdb386676d160b5c143fdfe68e
Bobby Boyd <bboyd2008@gmail.com>
2019-09-29 10:48:07 -0700

    404 redirect

--- pages/_error.js
+++ pages/_error.js
@@ -3,12 +3,20 @@ import React, { useEffect } from "react";
 function Error({ statusCode }) {
   useEffect(() => {
     console.log(window.location.href.split("/"));
+
+    if (window.location.href.split("/").length === 7) {
+      window.location.href =
+        "https://the-teacher-next-door.com/my-blog/" +
+        window.location.href.split("/")[6];
+    } else {
+      window.location.href = "https://the-teacher-next-door.com/my-blog";
+    }
   }, []);
   return (
     <p>
-      {statusCode
+      {/* {statusCode
         ? `An error ${statusCode} occurred on server`
-        : "An error occurred on client"}
+        : "An error occurred on client"} */}
     </p>
   );
 }
