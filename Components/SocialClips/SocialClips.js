60d9c178103a8cba4fa18fc85d2f6a7cff2f733d
Bobby Boyd <bboyd2008@gmail.com>
2019-09-29 12:09:44 -0700

    remove pinterest

--- Components/SocialClips/SocialClips.js
+++ Components/SocialClips/SocialClips.js
@@ -14,17 +14,7 @@ class SocialClips extends Component {
         <div className="container-fluid social-clips">
           <div className="container">
             <div className="columns">
-              <div className="column is-4">
-                <h2>Pinterest</h2>
-                <a
-                  data-pin-do="embedUser"
-                  data-pin-board-width="375"
-                  data-pin-scale-height="545"
-                  data-pin-scale-width="80"
-                  href="https://www.pinterest.com/TeacherNextDoor/"
-                ></a>
-              </div>
-              <div className="column is-4 facebook">
+              <div className="column is-6 facebook">
                 <h2>Facebook</h2>
                 <iframe
                   src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FTheTeacherNextDoor%2F&tabs=timeline&width=400&height=786&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=878288262298017"
@@ -41,7 +31,7 @@ class SocialClips extends Component {
                   allow="encrypted-media"
                 ></iframe>
               </div>
-              <div className="column is-4">
+              <div className="column is-6">
                 <h2>Instagram</h2>
                 <iframe
                   src="//lightwidget.com/widgets/f1774a3b24a655c084ebfe1ac596c67d.html"
