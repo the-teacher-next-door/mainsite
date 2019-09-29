4d9d1c458a63951519c99df301ca8ab3745678d8
Bobby Boyd <bboyd2008@gmail.com>
2019-09-29 11:17:18 -0700

    search update

--- pages/my-blog.js
+++ pages/my-blog.js
@@ -212,35 +212,37 @@ const Blogs = props => {
     return (
       <Layout>
         <div className="blogs">
-          <NavNext />
-          <IconNav />
-          <MiniConvert />
-          <div className="container blog-container padding-top-30">
-            <div className="columns is-multiline">
-              <div className="column is-12">
-                <h2>Search Results</h2>
-              </div>
-              {props.blogs.length > 0 ? (
-                props.blogs.map((blog, index) => {
-                  if (blog.live) {
-                    return (
-                      <div className="column is-3" key={index}>
-                        <Card
-                          title={blog.title}
-                          cleanTitle={blog.cleanTitle}
-                          img={blog.img}
-                          description={blog.description}
-                          category={blog.category.split(",")[0]}
-                        />
-                      </div>
-                    );
-                  }
-                })
-              ) : (
-                <div className="empty">
-                  <h2>Nothing matches that search</h2>
+          <div style={{ minHeight: "50vh", height: "100%" }}>
+            <NavNext />
+            <IconNav />
+            <MiniConvert />
+            <div className="container blog-container padding-top-30">
+              <div className="columns is-multiline">
+                <div className="column is-12">
+                  <h2>Search Results</h2>
                 </div>
-              )}
+                {props.blogs.length > 0 ? (
+                  props.blogs.map((blog, index) => {
+                    if (blog.live) {
+                      return (
+                        <div className="column is-3" key={index}>
+                          <Card
+                            title={blog.title}
+                            cleanTitle={blog.cleanTitle}
+                            img={blog.img}
+                            description={blog.description}
+                            category={blog.category.split(",")[0]}
+                          />
+                        </div>
+                      );
+                    }
+                  })
+                ) : (
+                  <div className="empty">
+                    <h2>Nothing matches that search</h2>
+                  </div>
+                )}
+              </div>
             </div>
           </div>
           <Footer />
