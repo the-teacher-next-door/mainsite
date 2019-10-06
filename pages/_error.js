9fbacf7bcbd88d84a6381c16d157deb5168f1b5a
Bobby Boyd <bboyd2008@gmail.com>
2019-10-06 16:53:46 -0700

    yep

--- pages/_error.js
+++ pages/_error.js
@@ -2,19 +2,30 @@ import React, { useEffect } from "react";
 
 function Error({ statusCode }) {
   useEffect(() => {
-    if (window.location.href.split("/").length === 7) {
-      let param = window.location.href.split("/")[6];
-      
-      window.location.href =
-        "https://the-teacher-next-door.com/my-blog/reading/" + param;
-    } else if (window.location.href.split("/").length === 6) {
-      let param = window.location.href.split("/")[5];
-      window.location.href =
-        "https://the-teacher-next-door.com/my-blog/" + param;
+    if (statusCode == 500) {
+      if (window.location.href.split("/").length === 7) {
+        let param = window.location.href.split("/")[6];
+        window.location.href =
+          "https://the-teacher-next-door.com/my-blog/" + param;
+      } else if (window.location.href.split("/").length === 6) {
+        let param = window.location.href.split("/")[5];
+        window.location.href =
+          "https://the-teacher-next-door.com/my-blog/reading" + param;
+      }
     } else {
-      window.location.href = "https://the-teacher-next-door.com/my-blog";
+      if (window.location.href.split("/").length === 7) {
+        let param = window.location.href.split("/")[6];
+
+        window.location.href =
+          "https://the-teacher-next-door.com/my-blog/reading/" + param;
+      } else if (window.location.href.split("/").length === 6) {
+        let param = window.location.href.split("/")[5];
+        window.location.href =
+          "https://the-teacher-next-door.com/my-blog/reading" + param;
+      } else {
+      }
     }
-  }, []);
+  }, [statusCode]);
   return (
     <p>
       {statusCode
